eaf3c7f4d1917eb5fb981d5dc9c1539eaedac885
Atick Faisal <atickfaisal@gmail.com>
2021-11-03 21:20:17 +0600

    Initial commit

---
+++ material-color/build.gradle.kts
@@ -0,0 +1,58 @@
+val composeVersion = "1.0.4"
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("maven-publish")
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 31
+
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = composeVersion
+    }
+}
+
+dependencies {
+    implementation("androidx.compose.ui:ui:$composeVersion")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "com.github.atick-faisal"
+                artifactId = "compose-material-color"
+                version = "1.0.0"
+
+                from(components["release"])
+            }
+        }
+    }
+}
---
+++ material-color/src/main/kotlin/ai/atick/material_color/MaterialColor.kt
@@ -0,0 +1,252 @@
+package ai.atick.material_color
+
+import androidx.compose.ui.graphics.Color
+
+@Suppress("UNUSED")
+object MaterialColor {
+
+    // ... Red
+    val Red50 = Color(0xFF_FFEBEE)
+    val Red100 = Color(0xFF_FFCDD2)
+    val Red200 = Color(0xFF_EF9A9A)
+    val Red300 = Color(0xFF_E57373)
+    val Red400 = Color(0xFF_EF5350)
+    val Red500 = Color(0xFF_F44336)
+    val Red600 = Color(0xFF_E53935)
+    val Red700 = Color(0xFF_D32F2F)
+    val Red800 = Color(0xFF_C62828)
+    val Red900 = Color(0xFF_B71C1C)
+
+    // ... Pink
+    val Pink50 = Color(0xFF_FCE4EC)
+    val Pink100 = Color(0xFF_F8BBD0)
+    val Pink200 = Color(0xFF_F48FB1)
+    val Pink300 = Color(0xFF_F06292)
+    val Pink400 = Color(0xFF_EC407A)
+    val Pink500 = Color(0xFF_E91E63)
+    val Pink600 = Color(0xFF_D81B60)
+    val Pink700 = Color(0xFF_C2185B)
+    val Pink800 = Color(0xFF_AD1457)
+    val Pink900 = Color(0xFF_880E4F)
+
+    // ...Purple
+    val Purple50 = Color(0xFF_F3E5F5)
+    val Purple100 = Color(0xFF_E1BEE7)
+    val Purple200 = Color(0xFF_CE93D8)
+    val Purple300 = Color(0xFF_BA68C8)
+    val Purple400 = Color(0xFF_AB47BC)
+    val Purple500 = Color(0xFF_9C27B0)
+    val Purple600 = Color(0xFF_8E24AA)
+    val Purple700 = Color(0xFF_7B1FA2)
+    val Purple800 = Color(0xFF_6A1B9A)
+    val Purple900 = Color(0xFF_4A148C)
+
+    // ... Deep Purple
+    val DeepPurple50 = Color(0xFF_EDE7F6)
+    val DeepPurple100 = Color(0xFF_D1C4E9)
+    val DeepPurple200 = Color(0xFF_B39DDB)
+    val DeepPurple300 = Color(0xFF_9575CD)
+    val DeepPurple400 = Color(0xFF_7E57C2)
+    val DeepPurple500 = Color(0xFF_673AB7)
+    val DeepPurple600 = Color(0xFF_5E35B1)
+    val DeepPurple700 = Color(0xFF_512DA8)
+    val DeepPurple800 = Color(0xFF_4527A0)
+    val DeepPurple900 = Color(0xFF_311B92)
+
+    // ... Indigo
+    val Indigo50 = Color(0xFF_E8EAF6)
+    val Indigo100 = Color(0xFF_C5CAE9)
+    val Indigo200 = Color(0xFF_9FA8DA)
+    val Indigo300 = Color(0xFF_7986CB)
+    val Indigo400 = Color(0xFF_5C6BC0)
+    val Indigo500 = Color(0xFF_3F51B5)
+    val Indigo600 = Color(0xFF_3949AB)
+    val Indigo700 = Color(0xFF_303F9F)
+    val Indigo800 = Color(0xFF_283593)
+    val Indigo900 = Color(0xFF_1A237E)
+
+    // ... Blue
+    val Blue50 = Color(0xFF_E3F2FD)
+    val Blue100 = Color(0xFF_BBDEFB)
+    val Blue200 = Color(0xFF_90CAF9)
+    val Blue300 = Color(0xFF_64B5F6)
+    val Blue400 = Color(0xFF_42A5F5)
+    val Blue500 = Color(0xFF_2196F3)
+    val Blue600 = Color(0xFF_1E88E5)
+    val Blue700 = Color(0xFF_1976D2)
+    val Blue800 = Color(0xFF_1565C0)
+    val Blue900 = Color(0xFF_0D47A1)
+
+    // ... LightBlue
+    val LightBlue50 = Color(0xFF_E1F5FE)
+    val LightBlue100 = Color(0xFF_B3E5FC)
+    val LightBlue200 = Color(0xFF_81D4FA)
+    val LightBlue300 = Color(0xFF_4FC3F7)
+    val LightBlue400 = Color(0xFF_29B6F6)
+    val LightBlue500 = Color(0xFF_03A9F4)
+    val LightBlue600 = Color(0xFF_039BE5)
+    val LightBlue700 = Color(0xFF_0288D1)
+    val LightBlue800 = Color(0xFF_0277BD)
+    val LightBlue900 = Color(0xFF_01579B)
+
+    // ... Cyan
+    val Cyan50 = Color(0xFF_E0F7FA)
+    val Cyan100 = Color(0xFF_B2EBF2)
+    val Cyan200 = Color(0xFF_80DEEA)
+    val Cyan300 = Color(0xFF_4DD0E1)
+    val Cyan400 = Color(0xFF_26C6DA)
+    val Cyan500 = Color(0xFF_00BCD4)
+    val Cyan600 = Color(0xFF_00ACC1)
+    val Cyan700 = Color(0xFF_0097A7)
+    val Cyan800 = Color(0xFF_00838F)
+    val Cyan900 = Color(0xFF_006064)
+
+    // ... Teal
+    val Teal50 = Color(0xFF_E0F2F1)
+    val Teal100 = Color(0xFF_B2DFDB)
+    val Teal200 = Color(0xFF_80CBC4)
+    val Teal300 = Color(0xFF_4DB6AC)
+    val Teal400 = Color(0xFF_26A69A)
+    val Teal500 = Color(0xFF_009688)
+    val Teal600 = Color(0xFF_00897B)
+    val Teal700 = Color(0xFF_00796B)
+    val Teal800 = Color(0xFF_00695C)
+    val Teal900 = Color(0xFF_004D40)
+
+    // ... Green
+    val Green50 = Color(0xFF_E8F5E9)
+    val Green100 = Color(0xFF_C8E6C9)
+    val Green200 = Color(0xFF_A5D6A7)
+    val Green300 = Color(0xFF_81C784)
+    val Green400 = Color(0xFF_66BB6A)
+    val Green500 = Color(0xFF_4CAF50)
+    val Green600 = Color(0xFF_43A047)
+    val Green700 = Color(0xFF_388E3C)
+    val Green800 = Color(0xFF_2E7D32)
+    val Green900 = Color(0xFF_1B5E20)
+
+    // ... Light Green
+    val LightGreen50 = Color(0xFF_F1F8E9)
+    val LightGreen100 = Color(0xFF_DCEDC8)
+    val LightGreen200 = Color(0xFF_C5E1A5)
+    val LightGreen300 = Color(0xFF_AED581)
+    val LightGreen400 = Color(0xFF_9CCC65)
+    val LightGreen500 = Color(0xFF_8BC34A)
+    val LightGreen600 = Color(0xFF_7CB342)
+    val LightGreen700 = Color(0xFF_689F38)
+    val LightGreen800 = Color(0xFF_558B2F)
+    val LightGreen900 = Color(0xFF_33691E)
+
+    // ... Lime
+    val Lime50 = Color(0xFF_F9FBE7)
+    val Lime100 = Color(0xFF_F0F4C3)
+    val Lime200 = Color(0xFF_E6EE9C)
+    val Lime300 = Color(0xFF_DCE775)
+    val Lime400 = Color(0xFF_D4E157)
+    val Lime500 = Color(0xFF_CDDC39)
+    val Lime600 = Color(0xFF_C0CA33)
+    val Lime700 = Color(0xFF_AFB42B)
+    val Lime800 = Color(0xFF_9E9D24)
+    val Lime900 = Color(0xFF_827717)
+
+    // ... Yellow
+    val Yellow50 = Color(0xFF_FFFDE7)
+    val Yellow100 = Color(0xFF_FFF9C4)
+    val Yellow200 = Color(0xFF_FFF59D)
+    val Yellow300 = Color(0xFF_FFF176)
+    val Yellow400 = Color(0xFF_FFEE58)
+    val Yellow500 = Color(0xFF_FFEB3B)
+    val Yellow600 = Color(0xFF_FDD835)
+    val Yellow700 = Color(0xFF_FBC02D)
+    val Yellow800 = Color(0xFF_F9A825)
+    val Yellow900 = Color(0xFF_F57F17)
+
+    // ... Amber
+    val Amber50 = Color(0xFF_FFF8E1)
+    val Amber100 = Color(0xFF_FFECB3)
+    val Amber200 = Color(0xFF_FFE082)
+    val Amber300 = Color(0xFF_FFD54F)
+    val Amber400 = Color(0xFF_FFCA28)
+    val Amber500 = Color(0xFF_FFC107)
+    val Amber600 = Color(0xFF_FFB300)
+    val Amber700 = Color(0xFF_FFA000)
+    val Amber800 = Color(0xFF_FF8F00)
+    val Amber900 = Color(0xFF_FF6F00)
+
+    // ... Orange
+    val Orange50 = Color(0xFF_FFF3E0)
+    val Orange100 = Color(0xFF_FFE0B2)
+    val Orange200 = Color(0xFF_FFCC80)
+    val Orange300 = Color(0xFF_FFB74D)
+    val Orange400 = Color(0xFF_FFA726)
+    val Orange500 = Color(0xFF_FF9800)
+    val Orange600 = Color(0xFF_FB8C00)
+    val Orange700 = Color(0xFF_F57C00)
+    val Orange800 = Color(0xFF_EF6C00)
+    val Orange900 = Color(0xFF_E65100)
+
+    // ... Deep Orange
+    val DeepOrange50 = Color(0xFF_FBE9E7)
+    val DeepOrange100 = Color(0xFF_FFCCBC)
+    val DeepOrange200 = Color(0xFF_FFAB91)
+    val DeepOrange300 = Color(0xFF_FF8A65)
+    val DeepOrange400 = Color(0xFF_FF7043)
+    val DeepOrange500 = Color(0xFF_FF5722)
+    val DeepOrange600 = Color(0xFF_F4511E)
+    val DeepOrange700 = Color(0xFF_E64A19)
+    val DeepOrange800 = Color(0xFF_D84315)
+    val DeepOrange900 = Color(0xFF_BF360C)
+
+    // ... Brown
+    val Brown50 = Color(0xFF_EFEBE9)
+    val Brown100 = Color(0xFF_D7CCC8)
+    val Brown200 = Color(0xFF_BCAAA4)
+    val Brown300 = Color(0xFF_A1887F)
+    val Brown400 = Color(0xFF_8D6E63)
+    val Brown500 = Color(0xFF_795548)
+    val Brown600 = Color(0xFF_6D4C41)
+    val Brown700 = Color(0xFF_5D4037)
+    val Brown800 = Color(0xFF_4E342E)
+    val Brown900 = Color(0xFF_3E2723)
+
+    // ... Gray
+    val Gray50 = Color(0xFF_FAFAFA)
+    val Gray100 = Color(0xFF_F5F5F5)
+    val Gray200 = Color(0xFF_EEEEEE)
+    val Gray300 = Color(0xFF_E0E0E0)
+    val Gray400 = Color(0xFF_BDBDBD)
+    val Gray500 = Color(0xFF_9E9E9E)
+    val Gray600 = Color(0xFF_757575)
+    val Gray700 = Color(0xFF_616161)
+    val Gray800 = Color(0xFF_424242)
+    val Gray900 = Color(0xFF_212121)
+
+    // ... Blue Gray
+    val BlueGray50 = Color(0xFF_ECEFF1)
+    val BlueGray100 = Color(0xFF_CFD8DC)
+    val BlueGray200 = Color(0xFF_B0BEC5)
+    val BlueGray300 = Color(0xFF_90A4AE)
+    val BlueGray400 = Color(0xFF_78909C)
+    val BlueGray500 = Color(0xFF_607D8B)
+    val BlueGray600 = Color(0xFF_546E7A)
+    val BlueGray700 = Color(0xFF_455A64)
+    val BlueGray800 = Color(0xFF_37474F)
+    val BlueGray900 = Color(0xFF_263238)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
