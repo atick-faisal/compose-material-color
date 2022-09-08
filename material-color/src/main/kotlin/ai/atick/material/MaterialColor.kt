754cfd18cb71255c832133bde96da40209cf0163
Atick Faisal <atickfaisal@gmail.com>
2022-09-09 01:04:41 +0300

    Add stable annotation to material colors

--- material-color/src/main/kotlin/ai/atick/material/MaterialColor.kt
+++ material-color/src/main/kotlin/ai/atick/material/MaterialColor.kt
@@ -1,321 +1,578 @@
 package ai.atick.material
 
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
 
 @Suppress("UNUSED")
 object MaterialColor {
 
     // ... Red
+    @Stable
     val Red50 = Color(0xFF_FFEBEE)
+    @Stable
     val Red100 = Color(0xFF_FFCDD2)
+    @Stable
     val Red200 = Color(0xFF_EF9A9A)
+    @Stable
     val Red300 = Color(0xFF_E57373)
+    @Stable
     val Red400 = Color(0xFF_EF5350)
+    @Stable
     val Red500 = Color(0xFF_F44336)
+    @Stable
     val Red600 = Color(0xFF_E53935)
+    @Stable
     val Red700 = Color(0xFF_D32F2F)
+    @Stable
     val Red800 = Color(0xFF_C62828)
+    @Stable
     val Red900 = Color(0xFF_B71C1C)
 
+    @Stable
     val RedA100 = Color(0xFF_FF8A80)
+    @Stable
     val RedA200 = Color(0xFF_FF5252)
+    @Stable
     val RedA400 = Color(0xFF_FF1744)
+    @Stable
     val RedA700 = Color(0xFF_D50000)
 
     // ... Pink
+    @Stable
     val Pink50 = Color(0xFF_FCE4EC)
+    @Stable
     val Pink100 = Color(0xFF_F8BBD0)
+    @Stable
     val Pink200 = Color(0xFF_F48FB1)
+    @Stable
     val Pink300 = Color(0xFF_F06292)
+    @Stable
     val Pink400 = Color(0xFF_EC407A)
+    @Stable
     val Pink500 = Color(0xFF_E91E63)
+    @Stable
     val Pink600 = Color(0xFF_D81B60)
+    @Stable
     val Pink700 = Color(0xFF_C2185B)
+    @Stable
     val Pink800 = Color(0xFF_AD1457)
+    @Stable
     val Pink900 = Color(0xFF_880E4F)
 
+    @Stable
     val PinkA100 = Color(0xFF_FF80AB)
+    @Stable
     val PinkA200 = Color(0xFF_FF4081)
+    @Stable
     val PinkA400 = Color(0xFF_F50057)
+    @Stable
     val PinkA700 = Color(0xFF_C51162)
 
 
     // ...Purple
+    @Stable
     val Purple50 = Color(0xFF_F3E5F5)
+    @Stable
     val Purple100 = Color(0xFF_E1BEE7)
+    @Stable
     val Purple200 = Color(0xFF_CE93D8)
+    @Stable
     val Purple300 = Color(0xFF_BA68C8)
+    @Stable
     val Purple400 = Color(0xFF_AB47BC)
+    @Stable
     val Purple500 = Color(0xFF_9C27B0)
+    @Stable
     val Purple600 = Color(0xFF_8E24AA)
+    @Stable
     val Purple700 = Color(0xFF_7B1FA2)
+    @Stable
     val Purple800 = Color(0xFF_6A1B9A)
+    @Stable
     val Purple900 = Color(0xFF_4A148C)
 
+    @Stable
     val PurpleA100 = Color(0xFF_EA80FC)
+    @Stable
     val PurpleA200 = Color(0xFF_E040FB)
+    @Stable
     val PurpleA400 = Color(0xFF_D500F9)
+    @Stable
     val PurpleA700 = Color(0xFF_AA00FF)
 
 
     // ... Deep Purple
+    @Stable
     val DeepPurple50 = Color(0xFF_EDE7F6)
+    @Stable
     val DeepPurple100 = Color(0xFF_D1C4E9)
+    @Stable
     val DeepPurple200 = Color(0xFF_B39DDB)
+    @Stable
     val DeepPurple300 = Color(0xFF_9575CD)
+    @Stable
     val DeepPurple400 = Color(0xFF_7E57C2)
+    @Stable
     val DeepPurple500 = Color(0xFF_673AB7)
+    @Stable
     val DeepPurple600 = Color(0xFF_5E35B1)
+    @Stable
     val DeepPurple700 = Color(0xFF_512DA8)
+    @Stable
     val DeepPurple800 = Color(0xFF_4527A0)
+    @Stable
     val DeepPurple900 = Color(0xFF_311B92)
 
+    @Stable
     val DeepPurpleA100 = Color(0xFF_B388FF)
+    @Stable
     val DeepPurpleA200 = Color(0xFF_7C4DFF)
+    @Stable
     val DeepPurpleA400 = Color(0xFF_651FFF)
+    @Stable
     val DeepPurpleA700 = Color(0xFF_6200EA)
 
     // ... Indigo
+    @Stable
     val Indigo50 = Color(0xFF_E8EAF6)
+    @Stable
     val Indigo100 = Color(0xFF_C5CAE9)
+    @Stable
     val Indigo200 = Color(0xFF_9FA8DA)
+    @Stable
     val Indigo300 = Color(0xFF_7986CB)
+    @Stable
     val Indigo400 = Color(0xFF_5C6BC0)
+    @Stable
     val Indigo500 = Color(0xFF_3F51B5)
+    @Stable
     val Indigo600 = Color(0xFF_3949AB)
+    @Stable
     val Indigo700 = Color(0xFF_303F9F)
+    @Stable
     val Indigo800 = Color(0xFF_283593)
+    @Stable
     val Indigo900 = Color(0xFF_1A237E)
 
+    @Stable
     val IndigoA100 = Color(0xFF_8C9EFF)
+    @Stable
     val IndigoA200 = Color(0xFF_536DFE)
+    @Stable
     val IndigoA400 = Color(0xFF_3D5AFE)
+    @Stable
     val IndigoA700 = Color(0xFF_304FFE)
 
     // ... Blue
+    @Stable
     val Blue50 = Color(0xFF_E3F2FD)
+    @Stable
     val Blue100 = Color(0xFF_BBDEFB)
+    @Stable
     val Blue200 = Color(0xFF_90CAF9)
+    @Stable
     val Blue300 = Color(0xFF_64B5F6)
+    @Stable
     val Blue400 = Color(0xFF_42A5F5)
+    @Stable
     val Blue500 = Color(0xFF_2196F3)
+    @Stable
     val Blue600 = Color(0xFF_1E88E5)
+    @Stable
     val Blue700 = Color(0xFF_1976D2)
+    @Stable
     val Blue800 = Color(0xFF_1565C0)
+    @Stable
     val Blue900 = Color(0xFF_0D47A1)
 
+    @Stable
     val BlueA100 = Color(0xFF_82B1FF)
+    @Stable
     val BlueA200 = Color(0xFF_448AFF)
+    @Stable
     val BlueA400 = Color(0xFF_2979FF)
+    @Stable
     val BlueA700 = Color(0xFF_2962FF)
 
     // ... LightBlue
+    @Stable
     val LightBlue50 = Color(0xFF_E1F5FE)
+    @Stable
     val LightBlue100 = Color(0xFF_B3E5FC)
+    @Stable
     val LightBlue200 = Color(0xFF_81D4FA)
+    @Stable
     val LightBlue300 = Color(0xFF_4FC3F7)
+    @Stable
     val LightBlue400 = Color(0xFF_29B6F6)
+    @Stable
     val LightBlue500 = Color(0xFF_03A9F4)
+    @Stable
     val LightBlue600 = Color(0xFF_039BE5)
+    @Stable
     val LightBlue700 = Color(0xFF_0288D1)
+    @Stable
     val LightBlue800 = Color(0xFF_0277BD)
+    @Stable
     val LightBlue900 = Color(0xFF_01579B)
 
+    @Stable
     val LightBlueA100 = Color(0xFF_80D8FF)
+    @Stable
     val LightBlueA200 = Color(0xFF_40C4FF)
+    @Stable
     val LightBlueA400 = Color(0xFF_00B0FF)
+    @Stable
     val LightBlueA700 = Color(0xFF_0091EA)
 
     // ... Cyan
+    @Stable
     val Cyan50 = Color(0xFF_E0F7FA)
+    @Stable
     val Cyan100 = Color(0xFF_B2EBF2)
+    @Stable
     val Cyan200 = Color(0xFF_80DEEA)
+    @Stable
     val Cyan300 = Color(0xFF_4DD0E1)
+    @Stable
     val Cyan400 = Color(0xFF_26C6DA)
+    @Stable
     val Cyan500 = Color(0xFF_00BCD4)
+    @Stable
     val Cyan600 = Color(0xFF_00ACC1)
+    @Stable
     val Cyan700 = Color(0xFF_0097A7)
+    @Stable
     val Cyan800 = Color(0xFF_00838F)
+    @Stable
     val Cyan900 = Color(0xFF_006064)
 
+    @Stable
     val CyanA100 = Color(0xFF_84FFFF)
+    @Stable
     val CyanA200 = Color(0xFF_18FFFF)
+    @Stable
     val CyanA400 = Color(0xFF_00E5FF)
+    @Stable
     val CyanA700 = Color(0xFF_00B8D4)
 
     // ... Teal
+    @Stable
     val Teal50 = Color(0xFF_E0F2F1)
+    @Stable
     val Teal100 = Color(0xFF_B2DFDB)
+    @Stable
     val Teal200 = Color(0xFF_80CBC4)
+    @Stable
     val Teal300 = Color(0xFF_4DB6AC)
+    @Stable
     val Teal400 = Color(0xFF_26A69A)
+    @Stable
     val Teal500 = Color(0xFF_009688)
+    @Stable
     val Teal600 = Color(0xFF_00897B)
+    @Stable
     val Teal700 = Color(0xFF_00796B)
+    @Stable
     val Teal800 = Color(0xFF_00695C)
+    @Stable
     val Teal900 = Color(0xFF_004D40)
 
+    @Stable
     val TealA100 = Color(0xFF_A7FFEB)
+    @Stable
     val TealA200 = Color(0xFF_64FFDA)
+    @Stable
     val TealA400 = Color(0xFF_1DE9B6)
+    @Stable
     val TealA700 = Color(0xFF_00BFA5)
 
     // ... Green
+    @Stable
     val Green50 = Color(0xFF_E8F5E9)
+    @Stable
     val Green100 = Color(0xFF_C8E6C9)
+    @Stable
     val Green200 = Color(0xFF_A5D6A7)
+    @Stable
     val Green300 = Color(0xFF_81C784)
+    @Stable
     val Green400 = Color(0xFF_66BB6A)
+    @Stable
     val Green500 = Color(0xFF_4CAF50)
+    @Stable
     val Green600 = Color(0xFF_43A047)
+    @Stable
     val Green700 = Color(0xFF_388E3C)
+    @Stable
     val Green800 = Color(0xFF_2E7D32)
+    @Stable
     val Green900 = Color(0xFF_1B5E20)
 
+    @Stable
     val GreenA100 = Color(0xFF_B9F6CA)
+    @Stable
     val GreenA200 = Color(0xFF_69F0AE)
+    @Stable
     val GreenA400 = Color(0xFF_00E676)
+    @Stable
     val GreenA700 = Color(0xFF_00C853)
 
     // ... Light Green
+    @Stable
     val LightGreen50 = Color(0xFF_F1F8E9)
+    @Stable
     val LightGreen100 = Color(0xFF_DCEDC8)
+    @Stable
     val LightGreen200 = Color(0xFF_C5E1A5)
+    @Stable
     val LightGreen300 = Color(0xFF_AED581)
+    @Stable
     val LightGreen400 = Color(0xFF_9CCC65)
+    @Stable
     val LightGreen500 = Color(0xFF_8BC34A)
+    @Stable
     val LightGreen600 = Color(0xFF_7CB342)
+    @Stable
     val LightGreen700 = Color(0xFF_689F38)
+    @Stable
     val LightGreen800 = Color(0xFF_558B2F)
+    @Stable
     val LightGreen900 = Color(0xFF_33691E)
 
+    @Stable
     val LightGreenA100 = Color(0xFF_CCFF90)
+    @Stable
     val LightGreenA200 = Color(0xFF_B2FF59)
+    @Stable
     val LightGreenA400 = Color(0xFF_76FF03)
+    @Stable
     val LightGreenA700 = Color(0xFF_64DD17)
 
     // ... Lime
+    @Stable
     val Lime50 = Color(0xFF_F9FBE7)
+    @Stable
     val Lime100 = Color(0xFF_F0F4C3)
+    @Stable
     val Lime200 = Color(0xFF_E6EE9C)
+    @Stable
     val Lime300 = Color(0xFF_DCE775)
+    @Stable
     val Lime400 = Color(0xFF_D4E157)
+    @Stable
     val Lime500 = Color(0xFF_CDDC39)
+    @Stable
     val Lime600 = Color(0xFF_C0CA33)
+    @Stable
     val Lime700 = Color(0xFF_AFB42B)
+    @Stable
     val Lime800 = Color(0xFF_9E9D24)
+    @Stable
     val Lime900 = Color(0xFF_827717)
 
+    @Stable
     val LimeA100 = Color(0xFF_F4FF81)
+    @Stable
     val LimeA200 = Color(0xFF_EEFF41)
+    @Stable
     val LimeA400 = Color(0xFF_C6FF00)
+    @Stable
     val LimeA700 = Color(0xFF_AEEA00)
 
     // ... Yellow
+    @Stable
     val Yellow50 = Color(0xFF_FFFDE7)
+    @Stable
     val Yellow100 = Color(0xFF_FFF9C4)
+    @Stable
     val Yellow200 = Color(0xFF_FFF59D)
+    @Stable
     val Yellow300 = Color(0xFF_FFF176)
+    @Stable
     val Yellow400 = Color(0xFF_FFEE58)
+    @Stable
     val Yellow500 = Color(0xFF_FFEB3B)
+    @Stable
     val Yellow600 = Color(0xFF_FDD835)
+    @Stable
     val Yellow700 = Color(0xFF_FBC02D)
+    @Stable
     val Yellow800 = Color(0xFF_F9A825)
+    @Stable
     val Yellow900 = Color(0xFF_F57F17)
 
+    @Stable
     val YellowA100 = Color(0xFF_FFFF8D)
+    @Stable
     val YellowA200 = Color(0xFF_FFFF00)
+    @Stable
     val YellowA400 = Color(0xFF_FFEA00)
+    @Stable
     val YellowA700 = Color(0xFF_FFD600)
 
     // ... Amber
+    @Stable
     val Amber50 = Color(0xFF_FFF8E1)
+    @Stable
     val Amber100 = Color(0xFF_FFECB3)
+    @Stable
     val Amber200 = Color(0xFF_FFE082)
+    @Stable
     val Amber300 = Color(0xFF_FFD54F)
+    @Stable
     val Amber400 = Color(0xFF_FFCA28)
+    @Stable
     val Amber500 = Color(0xFF_FFC107)
+    @Stable
     val Amber600 = Color(0xFF_FFB300)
+    @Stable
     val Amber700 = Color(0xFF_FFA000)
+    @Stable
     val Amber800 = Color(0xFF_FF8F00)
+    @Stable
     val Amber900 = Color(0xFF_FF6F00)
 
+    @Stable
     val AmberA100 = Color(0xFF_FFE57F)
+    @Stable
     val AmberA200 = Color(0xFF_FFD740)
+    @Stable
     val AmberA400 = Color(0xFF_FFC400)
+    @Stable
     val AmberA700 = Color(0xFF_FFAB00)
 
     // ... Orange
+    @Stable
     val Orange50 = Color(0xFF_FFF3E0)
+    @Stable
     val Orange100 = Color(0xFF_FFE0B2)
+    @Stable
     val Orange200 = Color(0xFF_FFCC80)
+    @Stable
     val Orange300 = Color(0xFF_FFB74D)
+    @Stable
     val Orange400 = Color(0xFF_FFA726)
+    @Stable
     val Orange500 = Color(0xFF_FF9800)
+    @Stable
     val Orange600 = Color(0xFF_FB8C00)
+    @Stable
     val Orange700 = Color(0xFF_F57C00)
+    @Stable
     val Orange800 = Color(0xFF_EF6C00)
+    @Stable
     val Orange900 = Color(0xFF_E65100)
 
+    @Stable
     val OrangeA100 = Color(0xFF_FFD180)
+    @Stable
     val OrangeA200 = Color(0xFF_FFAB40)
+    @Stable
     val OrangeA400 = Color(0xFF_FF9100)
+    @Stable
     val OrangeA700 = Color(0xFF_FF6D00)
 
     // ... Deep Orange
+    @Stable
     val DeepOrange50 = Color(0xFF_FBE9E7)
+    @Stable
     val DeepOrange100 = Color(0xFF_FFCCBC)
+    @Stable
     val DeepOrange200 = Color(0xFF_FFAB91)
+    @Stable
     val DeepOrange300 = Color(0xFF_FF8A65)
+    @Stable
     val DeepOrange400 = Color(0xFF_FF7043)
+    @Stable
     val DeepOrange500 = Color(0xFF_FF5722)
+    @Stable
     val DeepOrange600 = Color(0xFF_F4511E)
+    @Stable
     val DeepOrange700 = Color(0xFF_E64A19)
+    @Stable
     val DeepOrange800 = Color(0xFF_D84315)
+    @Stable
     val DeepOrange900 = Color(0xFF_BF360C)
 
+    @Stable
     val DeepOrangeA100 = Color(0xFF_FF9E80)
+    @Stable
     val DeepOrangeA200 = Color(0xFF_FF6E40)
+    @Stable
     val DeepOrangeA400 = Color(0xFF_FF3D00)
+    @Stable
     val DeepOrangeA700 = Color(0xFF_DD2C00)
 
     // ... Brown
+    @Stable
     val Brown50 = Color(0xFF_EFEBE9)
+    @Stable
     val Brown100 = Color(0xFF_D7CCC8)
+    @Stable
     val Brown200 = Color(0xFF_BCAAA4)
+    @Stable
     val Brown300 = Color(0xFF_A1887F)
+    @Stable
     val Brown400 = Color(0xFF_8D6E63)
+    @Stable
     val Brown500 = Color(0xFF_795548)
+    @Stable
     val Brown600 = Color(0xFF_6D4C41)
+    @Stable
     val Brown700 = Color(0xFF_5D4037)
+    @Stable
     val Brown800 = Color(0xFF_4E342E)
+    @Stable
     val Brown900 = Color(0xFF_3E2723)
 
     // ... Gray
+    @Stable
     val Gray50 = Color(0xFF_FAFAFA)
+    @Stable
     val Gray100 = Color(0xFF_F5F5F5)
+    @Stable
     val Gray200 = Color(0xFF_EEEEEE)
+    @Stable
     val Gray300 = Color(0xFF_E0E0E0)
+    @Stable
     val Gray400 = Color(0xFF_BDBDBD)
+    @Stable
     val Gray500 = Color(0xFF_9E9E9E)
+    @Stable
     val Gray600 = Color(0xFF_757575)
+    @Stable
     val Gray700 = Color(0xFF_616161)
+    @Stable
     val Gray800 = Color(0xFF_424242)
+    @Stable
     val Gray900 = Color(0xFF_212121)
 
     // ... Blue Gray
+    @Stable
     val BlueGray50 = Color(0xFF_ECEFF1)
+    @Stable
     val BlueGray100 = Color(0xFF_CFD8DC)
+    @Stable
     val BlueGray200 = Color(0xFF_B0BEC5)
+    @Stable
     val BlueGray300 = Color(0xFF_90A4AE)
+    @Stable
     val BlueGray400 = Color(0xFF_78909C)
+    @Stable
     val BlueGray500 = Color(0xFF_607D8B)
+    @Stable
     val BlueGray600 = Color(0xFF_546E7A)
+    @Stable
     val BlueGray700 = Color(0xFF_455A64)
+    @Stable
     val BlueGray800 = Color(0xFF_37474F)
+    @Stable
     val BlueGray900 = Color(0xFF_263238)
 
+    @Stable
     val Black = Color(0xFF_000000)
+    @Stable
     val White = Color(0xFF_FFFFFF)
 
 }
