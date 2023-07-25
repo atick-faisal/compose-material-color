8b5b74307abecf8b4b464eadb91af5cfd89e6d9d
Atick Faisal <atickfaisal@gmail.com>
2023-07-25 15:20:59 +0600

    Add Github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
 plugins {
-    id("com.android.library") version "7.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
-}
-
-task("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    id("com.android.library") version "8.0.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
--- material-color/build.gradle.kts
+++ material-color/build.gradle.kts
@@ -1,6 +1,6 @@
-val libraryVersion = "3.0.0"
-val composeVersion = "1.2.1"
-val composeCompilerVersion = "1.3.0"
+val libraryVersion = "4.0.0"
+val composeVersion = "1.4.3"
+val composeCompilerVersion = "1.5.0"
 
 plugins {
     id("com.android.library")
@@ -9,17 +9,18 @@ plugins {
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 31
+        targetSdk = 33
 
         consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
+            isMinifyEnabled = true
             proguardFiles(
                     getDefaultProguardFile("proguard-android-optimize.txt"),
                     "proguard-rules.pro"
@@ -28,12 +29,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -50,6 +51,8 @@ android {
             withJavadocJar()
         }
     }
+
+    namespace = "ai.atick.material"
 }
 
 dependencies {
