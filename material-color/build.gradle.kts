5fad114fef248032d97d5368db6bbc2740d0278b
Atick Faisal <atickfaisal@gmail.com>
2023-07-26 09:21:58 +0600

    Add spotless

--- material-color/build.gradle.kts
+++ material-color/build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Atick Faisal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 val libraryVersion = "4.0.0"
 val composeVersion = "1.4.3"
 val composeCompilerVersion = "1.5.0"
@@ -13,11 +29,10 @@ android {
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 33
-
         consumerProguardFiles("consumer-rules.pro")
     }
 
+    @Suppress("UnstableApiUsage")
     buildTypes {
         release {
             isMinifyEnabled = true
@@ -37,6 +52,7 @@ android {
         jvmTarget = "17"
     }
 
+    @Suppress("UnstableApiUsage")
     buildFeatures {
         compose = true
     }
